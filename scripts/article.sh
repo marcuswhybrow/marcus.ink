d55cf2b9a82846407d871904c3dfc43c1876803b
Marcus Whybrow <m@rcus.ink>
2020-05-11 08:38:16 +0100

    Open newly created articles in VSCode

--- scripts/article.sh
+++ scripts/article.sh
@@ -4,10 +4,14 @@ if [ $# -gt 0 ]; then
   path="$path/$1"
 fi
 
+file=$(date +"$path/%y%m%d.mdx")
+
 echo "export const meta = {
   title: 'Untitled',
   description: 'Undescribed',
   created: '$(date +'%Y-%m-%d')'
 }
 
-" >> $(date +"$path/%y%m%d.mdx")
+" >> $file
+
+code $file
